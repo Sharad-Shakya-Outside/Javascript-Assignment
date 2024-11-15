aa82ba85458ceaceb985c274627149629754e019
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-15 14:56:56 +0545

    WDT24-126 : Array and object defined and displayed in console

--- Day1/script.js
+++
@@ -1,5 +0,0 @@
-//Syntax to create array
-const numbers = [1, 2, 3, 4];
-
-//Using JavaScript Object to create array
-const fruits = new Array("apple", "grapes", "pineapple");
---
+++ Day1/script1.js
@@ -0,0 +1,14 @@
+//Defining array
+let arr = [10, 20, 30, 40, 50];
+console.table(arr); //Display the array in console in a form of table
+
+//Defining object
+let obj = {
+    name: 'John Cena',
+    department: 'Web',
+    age: '42',
+    greet: () => {
+        console.log('Hello!');
+    }
+};
+console.table(obj); //Display the array in console in a form of table
