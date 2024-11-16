3b990496e095a5fa86bf9f68909de6722ae38557
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-16 20:32:12 +0545

    WDT24-126 : scope for variables described

--- Day1/script4.js
+++ Day1/script4.js
@@ -1,19 +1,29 @@
 //How do var, let, and const differ in terms of scope and hoisting? Write a code differentiating them.
 
-console.log(a); //As var is hoisted to the top, this outputs undefined as var is declared at the top but not defined yet
+// console.log(a); //As var is hoisted to the top, this outputs undefined as var is declared at the top but not defined yet
 
-console.log(b); //Uncaught reference error as variable defined by let is not hoisted
+// console.log(b); //Uncaught reference error as variable defined by let is not hoisted
 
-console.log(c); //Uncaught reference error as variable defined by const is not hoisted
+// console.log(c); //Uncaught reference error as variable defined by const is not hoisted
 
 var a = 5; //Variable is finally defined
 
 console.log(a); //Now the output is 5 instead of undefined
 
-let b = 6; 
-const c = 7;
+const c = 7; //variables defind outside block scope
+let b = 10; //variables defind outside block scope
 
-console.log(b); //No errors here as variables are already defined and declared above
+console.log(b); //Here, output is 10
+
+{
+    //variables defined inside block scope
+    let b = 6; 
+
+    console.log(b); //Here, output is 6
+    console.log(c);
+}
+
+console.log(b); //No errors here as variables are already defined and declared above. The output is 10 here again.
 console.log(c); //No errors here as variables are already defined and declared above
 
 hoistingExample1(); //The function is executed as it is declared at the top
