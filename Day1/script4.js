b13fab4643b8f94cccc1c720b30e6eae28497abc
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-16 20:26:56 +0545

    WDT24-126 : Comments for exercise 4 added to describe hoisting

--- Day1/script4.js
+++ Day1/script4.js
@@ -1,31 +1,37 @@
-console.log(a);
+//How do var, let, and const differ in terms of scope and hoisting? Write a code differentiating them.
 
-console.log(b);
+console.log(a); //As var is hoisted to the top, this outputs undefined as var is declared at the top but not defined yet
 
-console.log(c);
+console.log(b); //Uncaught reference error as variable defined by let is not hoisted
 
-var a = 5;
+console.log(c); //Uncaught reference error as variable defined by const is not hoisted
 
-console.log(a);
+var a = 5; //Variable is finally defined
 
-let b = 6;
+console.log(a); //Now the output is 5 instead of undefined
+
+let b = 6; 
 const c = 7;
 
-console.log(b);
-console.log(c);
+console.log(b); //No errors here as variables are already defined and declared above
+console.log(c); //No errors here as variables are already defined and declared above
 
-hoistingExample1();
+hoistingExample1(); //The function is executed as it is declared at the top
 
 function hoistingExample1() {
+    //This type of function is always hoisted at the top
+
     let i = 1;
     console.log(i);
 }
 
-hoistingExample2();
+hoistingExample2(); //Uncaught reference error
 
 const hoistingExample2 = () => {
+    //Such arrow functions defined by let or const are not hoisted.
+
     let j = 2;
     console.log(j);
 }
 
-hoistingExample2();
+hoistingExample2(); //Finally executed as this is called after the function's declaration
