94d7736806c9e6ab99a979e039ead26b89173ff4
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-16 22:09:52 +0545

    WDT24-133 : Day 2 question 4 initialized

---
+++ Day2/script4.js
@@ -0,0 +1,6 @@
+/*
+Create a new array element.
+    a. Create a new array with the multiplication of 5
+    b. Create a new array finding the maximum number of new array (task number 4.a)
+    c. Also list out the even numbers of both new and original array.
+*/
