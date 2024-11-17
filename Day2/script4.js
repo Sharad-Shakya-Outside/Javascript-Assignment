3e113caad290633d9d52b97b47bd19700db3cb94
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-17 16:00:26 +0545

    WDT24-133 : array manipulated using map, filter and reduce

--- Day2/script4.js
+++ Day2/script4.js
@@ -4,3 +4,30 @@ Create a new array element.
     b. Create a new array finding the maximum number of new array (task number 4.a)
     c. Also list out the even numbers of both new and original array.
 */
+
+let integers = [1, 4, 3, 6, 2, 15, 10, 14]; //Array created
+
+console.table(integers); //Original array is displayed in the console
+
+//Using map to multiply the elements of array by 5
+let multiplicationOfFive = integers.map((integer, index) => {
+
+    return integer * 5; //Multiplies elements in the array by 5
+})
+console.table(multiplicationOfFive); //Output: [5, 20, 15, 30, 10, 75, 50, 70]
+
+//Using reduce function to find the greatest number in the new array
+let maximumNumber = multiplicationOfFive.reduce((acc, item) => {
+
+    acc = item > acc ? item : acc; //In every iteration, accumulator stores the greater element between the current value of accumulator and current element in the array
+
+    return acc;
+}, 0)
+console.log("The maximum number in the new array is: ", maximumNumber);
+
+//Using filter method to find the even numbers
+let evenNumbersOfIntegers = integers.filter((x) => x % 2 === 0);
+console.table(evenNumbersOfIntegers)
+
+let evenNumbersOfFiveTimesIntegers = multiplicationOfFive.filter((x) => x % 2 === 0);
+console.table(evenNumbersOfFiveTimesIntegers);
