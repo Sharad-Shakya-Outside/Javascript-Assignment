97f3b4d07f3daa45bccc19c6ae6a8e49547fc667
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-19 16:36:25 +0545

    WDT24-145 : Feature: Time displayed in two digits

--- Day3/DigitalClock/script.js
+++ Day3/DigitalClock/script.js
@@ -1,34 +1,42 @@
 let hours = document.querySelector('#hours');
 let minutes = document.querySelector('#minutes');
 let seconds = document.querySelector('#seconds');
-let beforeDigit = document.querySelectorAll('#two-digit')
+let secondsDigit = document.querySelector('#seconds-digit');
+let minutesDigit = document.querySelector('#minutes-digit');
+let hoursDigit = document.querySelector('#hours-digit');
 
-const convertToTwoDigits = (time) => {
-    if (time < 10) {
-        beforeDigit.forEach((tenthDigit) => {
-            tenthDigit.innerText = 0;
-        })
-    }
+const convertToTwoDigits = (time, tenthDigit) => {
+    time < 9 || time === 60 ? tenthDigit.innerText = 0 : tenthDigit.innerText = "";
 }
+
+convertToTwoDigits(seconds.innerText, secondsDigit);
+convertToTwoDigits(minutes.innerText, minutesDigit);
+convertToTwoDigits(hours.innerText, hoursDigit);
 
 const digitalClockTime = setInterval(() => {
     if (seconds.innerText < 59) {
-        convertToTwoDigits(seconds.innerText);
+        convertToTwoDigits(seconds.innerText, secondsDigit);
         seconds.innerText++;
+
     } else {
         seconds.innerText = 0;
+        convertToTwoDigits(seconds.innerText, secondsDigit);
 
         if (minutes.innerText < 59) {
-            convertToTwoDigits(minutes.innerText);
+            convertToTwoDigits(minutes.innerText, minutesDigit);
             minutes.innerText++;
+
         } else {
             minutes.innerText = 0;
-            
+            convertToTwoDigits(minutes.innerText, minutesDigit);
+
             if (hours.innerText < 23) {
-                convertToTwoDigits(hours.innerText);
+                convertToTwoDigits(hours.innerText, hoursDigit);
                 hours.innerText++;
+
             } else {
                 hours.innerText = 0;
+                convertToTwoDigits(hours.innerText, hoursDigit);
             }
         }
     }
