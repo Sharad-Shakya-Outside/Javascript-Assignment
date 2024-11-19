1186ccdb8b73fda510ed17d223f615d5ad4f7719
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-19 16:44:17 +0545

    WDT24-145 : Comments added for description

--- Day3/DigitalClock/script.js
+++ Day3/DigitalClock/script.js
@@ -1,3 +1,8 @@
+/*
+Create a real Digital Clock
+*/
+
+//Variables defined for selecting the time elements
 let hours = document.querySelector('#hours');
 let minutes = document.querySelector('#minutes');
 let seconds = document.querySelector('#seconds');
@@ -5,37 +10,40 @@ let secondsDigit = document.querySelector('#seconds-digit');
 let minutesDigit = document.querySelector('#minutes-digit');
 let hoursDigit = document.querySelector('#hours-digit');
 
+//Function to make all one digit numbers to two digit numbers by adding a 0 before the time element
 const convertToTwoDigits = (time, tenthDigit) => {
     time < 9 || time === 60 ? tenthDigit.innerText = 0 : tenthDigit.innerText = "";
 }
 
+//Function called early to convert any existing time to two digits
 convertToTwoDigits(seconds.innerText, secondsDigit);
 convertToTwoDigits(minutes.innerText, minutesDigit);
 convertToTwoDigits(hours.innerText, hoursDigit);
 
+//setInterval to increment time elements
 const digitalClockTime = setInterval(() => {
     if (seconds.innerText < 59) {
         convertToTwoDigits(seconds.innerText, secondsDigit);
-        seconds.innerText++;
+        seconds.innerText++; //increments seconds
 
     } else {
-        seconds.innerText = 0;
+        seconds.innerText = 0; //resets seconds
         convertToTwoDigits(seconds.innerText, secondsDigit);
 
         if (minutes.innerText < 59) {
             convertToTwoDigits(minutes.innerText, minutesDigit);
-            minutes.innerText++;
+            minutes.innerText++; //increments minutes if 60 seconds pass
 
         } else {
-            minutes.innerText = 0;
+            minutes.innerText = 0; //resets minutes on 60 minutes
             convertToTwoDigits(minutes.innerText, minutesDigit);
 
             if (hours.innerText < 23) {
                 convertToTwoDigits(hours.innerText, hoursDigit);
-                hours.innerText++;
+                hours.innerText++; //increments minutes if 60 minutes pass
 
             } else {
-                hours.innerText = 0;
+                hours.innerText = 0; //resets minutes on 24 hours
                 convertToTwoDigits(hours.innerText, hoursDigit);
             }
         }
