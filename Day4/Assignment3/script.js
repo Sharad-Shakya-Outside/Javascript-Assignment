3aeb870266a435571897dc0ba3af345aa6a8c707
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-20 12:55:18 +0545

    WDT24-151 : Throttle functionality to delay button action added

--- Day4/Assignment3/script.js
+++ Day4/Assignment3/script.js
@@ -1,3 +1,31 @@
 /*
 Apply the throttling technique in a button. The button should be pressed only once every 1 second.
 */
+
+let shootBtn = document.querySelector('.shoot');
+
+//Throttle function implementation
+function throttle(func, delay) {
+    let shouldWait = false;
+
+    return (...args) => {
+        if (shouldWait) return;
+
+        func(...args);
+        shouldWait = true;
+
+        setTimeout(() => {
+            shouldWait = false;
+            shootBtn.disabled = false;
+            shootBtn.style.backgroundColor = 'blue';
+        }, delay)
+    }
+}
+
+
+//Button shoot action
+shootBtn.addEventListener("click", throttle(() => {
+    console.log('Button is pressed');
+    shootBtn.disabled = false;
+    shootBtn.style.backgroundColor = 'red';
+}, 2000));
