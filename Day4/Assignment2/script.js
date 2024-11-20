a50ffd1c472105d36c38efcba5e3f04c8d0dd6a3
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-20 12:08:53 +0545

    WDT24-151 : Console on resize functionality added

--- Day4/Assignment2/script.js
+++ Day4/Assignment2/script.js
@@ -1,3 +1,15 @@
 /*
 Apply the debouncing technique on resize event. After the end of resize throw a console message saying, “Window is resized”.
 */
+
+function debounce(func) {
+    var timer;
+    return function(event) {
+        if(timer) clearTimeout(timer);
+        timer = setTimeout(func, 400, event);
+    }
+}
+
+window.addEventListener("resize", debounce(function(e) {
+    console.log('Window is resized');
+}))
